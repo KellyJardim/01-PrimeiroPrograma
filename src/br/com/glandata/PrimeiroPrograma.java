09a1ac5bef71962d25dab44afa9018f93c23f01e
Kelly Jardim <kelly.c.jardim@gmail.com>
2022-05-18 20:23:38 -0300

    Alteração do Sysout

--- src/br/com/glandata/PrimeiroPrograma.java
+++ src/br/com/glandata/PrimeiroPrograma.java
@@ -3,6 +3,6 @@ package br.com.glandata;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World!");
+	System.out.println("Olá Glan Data!");
 	}
 }
